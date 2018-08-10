3c50f8dfc3f8488da1043b16a61e5830d3105c91
tum0xa <timofey.samodurov@yandex.ru>
2018-08-10 23:56:02 +0500

    Added verbose names in model Theme

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -11,15 +11,18 @@ class Theme(models.Model):
     name = models.CharField(default='UnknownTheme', # Название темы
                             max_length=255,
                             unique=True,
-                            blank=False
+                            blank=False,
+                            verbose_name="Название темы"
                             )
     active = models.BooleanField(default=True,      # Показать/Скрыть тему
                                  blank=True,
+                                 verbose_name="Отображать тему и связанные посты в блоге"
                                  )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = "тему"
-        verbose_name_plural = "темы"
+
+        verbose_name = "Theme"
+        # verbose_name_plural = ""
