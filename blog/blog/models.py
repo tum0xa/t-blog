c5b3297ebad127339a68b396ba3ce1fad7ea377c
tum0xa <timofey.samodurov@yandex.ru>
2018-08-10 23:19:56 +0500

    Added model 'Themes'

---
+++ blog/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Theme(models.Model):
+
+    """
+    Class Theme
+    Темы постов
+    """
+
+    name = models.CharField(default='UnknownTheme', # Название темы
+                            max_length=255,
+                            unique=True,
+                            blank=False
+                            )
+    active = models.BooleanField(default=True,      # Показать/Скрыть тему
+                                 blank=True,
+                                 )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "тема"
+        verbose_name_plural = "темы"
