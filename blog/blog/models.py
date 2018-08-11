8b511bf749e7e266f0e1937025b7c3f1576f6a84
tum0xa <timofey.samodurov@yandex.ru>
2018-08-11 08:33:09 +0500

    Themes list (from db) added to navbar

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -14,10 +14,20 @@ class Theme(models.Model):
                             blank=False,
                             verbose_name="Название темы"
                             )
+    slug = models.SlugField(default='unknowntheme',
+                            max_length=64,
+                            blank=False,
+                            verbose_name="Относительный url-адрес"
+                            )
     active = models.BooleanField(default=True,      # Показать/Скрыть тему
                                  blank=True,
                                  verbose_name="Отображать тему и связанные посты в блоге"
                                  )
+    short_description = models.TextField(default='Short description of the theme',
+                                   unique=False,
+                                   blank=True,
+                                   verbose_name="Короткое описание темы"
+                                         )
 
     def __str__(self):
         return self.name
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import Theme
+
 
 def index(request):
-    return render(request, 'blog/index.html')
+    themes_list = Theme.objects.all()
+    context = {'themes_list': themes_list}
+    return render(request, 'blog/index.html', context)
